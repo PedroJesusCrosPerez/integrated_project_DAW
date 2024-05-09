465db8c8de9140973d71f5f2ced582642ef32d68
Pedro Jesus Cros Perez <pcroper1909@g.educaand.es>
2024-05-09 22:55:07 +0200

    deleting repos

--- src/main/java/app/proyecto/repository/UserAllRepository.java
+++
@@ -1,9 +0,0 @@
-package app.proyecto.repository;
-
-import app.proyecto.entity.UserAll;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserAllRepository extends JpaRepository<UserAll, Integer> {
-}
