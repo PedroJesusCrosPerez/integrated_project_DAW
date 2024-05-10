c4fb9a8ec1da0f8c74cdf12c4967db89e07d2390
pedro.cros <pedro.cros@nter.es>
2024-05-10 09:45:18 +0200

    adding line to productRepo

--- src/main/java/app/proyecto/repository/ProductRepository.java
+++ src/main/java/app/proyecto/repository/ProductRepository.java
@@ -4,6 +4,12 @@ import app.proyecto.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // TODO add year column
+    List<Product> findAllByYear(Integer year);
+
 }
